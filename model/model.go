cd3d95d078b8a465d18cf9162d28101fb7e94627
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-22 13:33:40 +0000

    fix: 添加stun测试可用的服务器地址

--- model/model.go
+++ model/model.go
@@ -20,21 +20,40 @@ func GetDefaultServers(IPVersion string) []string {
 	case "ipv6":
 		return []string{
 			"stun.hot-chilli.net:3478",
-			"[2a01:4f8:242:56ca::2]:3478",
+			"stun.ipfire.org:3478",
+			"stun.flashdance.cx:3478",
+			"stun.cloudflare.com:3478",
+			"stun.f.haeder.net:3478",
+			"stun.l.google.com:19302",
 		}
 	case "ipv4":
 		return []string{
 			"stun.voipgate.com:3478",
 			"stun.miwifi.com:3478",
+			"stun.fitauto.ru:3478",
+			"stun.internetcalls.com:3478",
+			"stun.voip.aebc.com:3478",
+			"stun.voipbuster.com:3478",
+			"stun.voipstunt.com:3478",
+			"stun.hot-chilli.net:3478",
 			"stunserver.stunprotocol.org:3478",
 		}
 	default:
 		return []string{
 			"stun.voipgate.com:3478",
 			"stun.miwifi.com:3478",
-			"stunserver.stunprotocol.org:3478",
+			"stun.fitauto.ru:3478",
+			"stun.internetcalls.com:3478",
+			"stun.voip.aebc.com:3478",
+			"stun.voipbuster.com:3478",
+			"stun.voipstunt.com:3478",
 			"stun.hot-chilli.net:3478",
-			"[2a01:4f8:242:56ca::2]:3478",
+			"stunserver.stunprotocol.org:3478",
+			"stun.l.google.com:19302",
+			"stun.ipfire.org:3478",
+			"stun.flashdance.cx:3478",
+			"stun.cloudflare.com:3478",
+			"stun.f.haeder.net:3478",
 		}
 	}
 }
